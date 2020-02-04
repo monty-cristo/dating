d54a7d4cebf9f74fdedbe7ed1773c4e04fed7f94
Jonah Ruymbeeck <Jonah.Ruymbeeck@ad.core.vdabantwerpen.intra>
2020-02-04 09:50:53 +0100

    Moved to modules

---
+++ js/methods/deleteProfile.js
@@ -0,0 +1,39 @@
+"use strict";
+async function deleteProfile() {
+  const wachtwoordPrompt = prompt("Voer uw wachtwoord in");
+
+  try {
+    const id = sessionStorage.getItem("user");
+    let url = `https://scrumserver.tenobe.org/scrum/api/profiel/read_one.php?id=${id}`;
+
+    let response = await fetch(url);
+    let { wachtwoord } = await response.json();
+
+    if (wachtwoord === wachtwoordPrompt) {
+      const data = {
+        id
+      };
+
+      url = "https://scrumserver.tenobe.org/scrum/api/profiel/delete.php";
+
+      const request = new Request(url, {
+        method: "DELETE",
+        body: JSON.stringify(data),
+        headers: new Headers({
+          "Content-Type": "application/json"
+        })
+      });
+
+      console.log(`Deleting user with id: ${id}`);
+
+      response = await fetch(request);
+      const message = await response.json();
+      console.log(message);
+      window.location.href = "/";
+    }
+  } catch ({ message }) {
+    alert(message);
+  }
+}
+
+export default deleteProfile;
