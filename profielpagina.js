78c70d7ececea99a878752ce03db42dddb7255a4
AugustynsWim <60511673+AugustynsWim@users.noreply.github.com>
2020-02-05 08:58:00 +0100

    favorietenknop

---
+++ profielpagina.js
@@ -0,0 +1,109 @@
+"use strict";
+
+
+let profielId = 3 //Math.floor(Math.random() * 4)+1; random profiel van 0 - 4
+
+//velden invullen
+window.onload = function () {
+
+    let profielData;
+
+    let url = 'https://scrumserver.tenobe.org/scrum/api/profiel/read_one.php?id=' + profielId;
+
+    fetch(url)
+        .then(function (resp) { return resp.json(); })
+        .then(function (data) {
+
+            profielData = data;
+
+            document.getElementById('detailNick').value = profielData.nickname;
+            document.getElementById('detailFnaam').value = profielData.familienaam;
+            document.getElementById('detailVnaam').value = profielData.voornaam;
+            document.getElementById('detailGeboortedatum').value = profielData.geboortedatum;
+            document.getElementById('detailHaarkleur').value = profielData.haarkleur;
+            document.getElementById('detailBeroep').value = profielData.beroep;
+            document.getElementById('detailEmail').value = profielData.email;
+            document.getElementById('detailLovecoins').value = profielData.lovecoins;
+            document.getElementById('detailFoto').setAttribute('src', 'https://scrumserver.tenobe.org/scrum/img/' + profielData.foto);
+            document.getElementById('detailFoto').setAttribute('alt', 'foto van ' + profielData.voornaam + ' ' + profielData.familienaam);
+            document.getElementById('profielVan').innerText = 'Details van ' + profielData.voornaam + ' ' + profielData.familienaam;
+            document.getElementById('detailSexe').value = profielData.sexe;
+            document.getElementById('detailOogkleur').value = profielData.oogkleur;
+            document.getElementById('detailGewicht').value = profielData.gewicht;
+            document.getElementById('detailGrootte').value = profielData.grootte;
+            document.getElementById('detailWachtWoord').value = profielData.wachtwoord;
+
+
+        })
+        .catch(function (error) { console.log(error); });
+
+
+    }
+
+//favorieten teovoegen
+let ingelogdId = 7;
+let favorietPersoon = false;
+let rooturl = 'https://scrumserver.tenobe.org/scrum/api';
+console.log(ingelogdId, profielId, 1, rooturl)
+document.getElementById('buttonLike').onclick = function (){
+    console.log(ingelogdId, profielId, 2)
+    favorietPersoon = true;
+    console.log(ingelogdId, profielId, 2, favorietPersoon)
+    let mijnId =  1; 
+    let anderId =  2;
+    let url=rooturl+'/favoriet/like.php';
+    //LET OP : rooturl = https://scrumserver.tenobe.org/scrum/api
+    let data = {
+        mijnId: mijnId,
+        anderId: anderId
+    }
+    var request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+    
+    fetch(request)
+        .then( function (resp)  { return resp.json(); })
+        .then( function (data)  { console.log(data);  })
+        .catch(function (error) { console.log(error); });
+
+};
+console.log(favorietPersoon)
+if (favorietPersoon) {document.getElementById('buttonLike').innerText= "Je hebt deze persoon een like gegeven."};
+
+
+//lovecoins spenderen om extra informatie te krijgen
+
+/*berichten sturen
+
+document.getElementById('knop22').addEventListener('click', function (e) {  
+    let vanId =  document.getElementById('input22_1').value; 
+    let naarId =  document.getElementById('input22_2').value; 
+    let bericht =  document.getElementById('input22_3').value; 
+
+    let url=rooturl+'/bericht/post.php';
+    //LET OP : rooturl = https://scrumserver.tenobe.org/scrum/api
+    let data = {
+        vanId:vanId,
+        naarId:naarId,
+        bericht:bericht,
+        status:"verzonden"
+    }
+
+    var request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    });
+
+    fetch(request)
+        .then( function (resp)  { return resp.json(); })
+        .then( function (data)  { console.log(data);  })
+        .catch(function (error) { console.log(error); });
+});
+*/
