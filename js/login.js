4d1a9cc085648c579ae044839ca84efdef1d61c9
Jonah Ruymbeeck <Jonah.Ruymbeeck@ad.core.vdabantwerpen.intra>
2020-02-05 09:27:22 +0100

    Fixed login I hope

---
+++ js/login.js
@@ -0,0 +1,36 @@
+"use strict";
+window.onload = function() {
+  document.getElementById("btnLogin").addEventListener("click", async () => {
+    const nickname = document.getElementById("loginNick").value;
+    const wachtwoord = document.getElementById("loginPas").value;
+
+    const url =
+      "https://scrumserver.tenobe.org/scrum/api/profiel/authenticate.php";
+
+    const data = {
+      nickname,
+      wachtwoord
+    };
+
+    const request = new Request(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+
+    try {
+      const response = await fetch(request);
+      const { message, id } = await response.json();
+
+      if (message == "Authorized") {
+        console.log("Reactie van backend API : Correcte gegevens");
+        sessionStorage.setItem("user", id);
+        window.location.href = `profile.html?id=${id}`;
+      }
+    } catch ({ message }) {
+      alert(message);
+    }
+  });
+};
