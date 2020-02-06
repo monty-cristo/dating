c9fb7ad8eca7f3aa03fd07381721a4ff9c0d0b83
Jonah Ruymbeeck <ruymbeeckjonah@gmail.com>
2020-02-06 21:31:45 +0100

    Added chat

--- js/chat.js
+++ js/chat.js
@@ -1,12 +1,16 @@
 "use strict";
 import { html, render, Component } from "../vendor/js/preact.js";
 
+if(document.referrer != window.location && performance.navigation.type != 1) {
+  sessionStorage.removeItem("selectedUser");
+}
+
 class ContactItem extends Component {
   changeContact = selectedUser => {
     this.props.changeContact(selectedUser);
   };
 
-  render({ contact: { familienaam, voornaam, foto }, index, selected }) {
+  render({ contact: { familienaam, voornaam, foto, email }, index, selected }) {
     return html`
       <li
         class="contactItem ${selected == 1 ? "selected" : ""}"
@@ -15,7 +19,7 @@ class ContactItem extends Component {
         <img src=https://scrumserver.tenobe.org/scrum/img/${foto} />
         <div>
           <span>${`${familienaam} ${voornaam}`}</span>
-          <span>gisteren</span>
+          <span>${email}</span>
         </div>
       </li>
     `;
@@ -48,10 +52,12 @@ class Chat extends Component {
 
   onTextSend = async () => {
     await this.props.onTextSend();
-    //TODO: clear text from chat input
+    this.inputBoxRef.focus();
   };
 
-  render({ conversation = [], text = "" }) {
+  setInputBoxRef = dom => (this.inputBoxRef = dom);
+
+  render({ conversation = [], text }) {
     return html`
       <div class="chat">
         <${Conversation} conversation=${conversation} />
@@ -61,6 +67,7 @@ class Chat extends Component {
             value=${text}
             type="text"
             placeholder="Stuur text..."
+            ref=${this.setInputBoxRef}
           />
           <button onclick=${this.onTextSend} type="button">Send</button>
         </div>
@@ -72,11 +79,15 @@ class Chat extends Component {
 class Conversation extends Component {
   render({ conversation }) {
     return html`
-      <ul class="conversation">
+      <ul class="conversation" ref=${this.setChatBoxRef}>
         ${conversation.map(
-          ({ bericht, benIkZender }) =>
+          ({ bericht, benIkZender }, index) =>
             html`
-              <${Message} message=${bericht} benIkZender=${benIkZender} />
+              <${Message}
+                message=${bericht}
+                benIkZender=${benIkZender}
+                isLast=${index === conversation.length - 1}
+              />
             `
         )}
       </ul>
@@ -85,105 +96,161 @@ class Conversation extends Component {
 }
 
 class Message extends Component {
-  render({ message, benIkZender }) {
+  componentDidMount() {
+    if (this.props.isLast) this.chatBoxRef.scrollIntoView();
+  }
+
+  componentDidUpdate() {
+    if (this.props.isLast) this.chatBoxRef.scrollIntoView();
+  }
+
+  setChatBoxRef = dom => (this.chatBoxRef = dom);
+
+  render({ message, benIkZender, isLast }) {
+    if (isLast) {
+      return html`
+        <li
+          class="message ${benIkZender == 1 ? "zender" : ""}"
+          ref=${this.setChatBoxRef}
+        >
+          ${message}
+        </li>
+      `;
+    }
+
     return html`
-      <li class="message ${benIkZender == 1 ? "zender" : ""}">${message}</li>
+      <li class="message ${benIkZender == 1 ? "zender" : ""}">
+        ${message}
+      </li>
     `;
   }
 }
 
 class App extends Component {
+  state = {
+    text: "",
+    selectedUser: 0
+  };
+
   constructor() {
     super();
-
-    const state = {
-      text: "",
-      contacts: []
-    };
-
     //Check to see if the user has a selected user in session.
+    this.checkSessionUser();
+  }
+
+  async componentDidMount() {
+    const initialConversations = await this.getConversations();
+
+    let initialUserIds = initialConversations.map(
+      ([{ partnerId }]) => partnerId
+    );
+
+    const [conversations, selectedUser, userIds] = this.createNewConversation(
+      initialConversations,
+      initialUserIds
+    );
+
+    const contacts = await this.getContacts(userIds);
+
+    this.setState({
+      conversations,
+      contacts,
+      selectedUser
+    });
+  }
+
+  checkSessionUser = () => {
     if (sessionStorage.getItem("selectedUser")) {
       this.state = {
-        ...state,
+        ...this.state,
         selectedUser: sessionStorage.getItem("selectedUser")
       };
-    } else {
-      this.state = {
-        ...state,
-        selectedUser: 0
-      };
     }
-  }
-
-  componentDidMount() {
-    this.getConversations();
-  }
+  };
 
   getConversations = async () => {
     const id = sessionStorage.getItem("user");
-    const url = new URL(window.location.href);
-    let selectedUser = this.state.selectedUser;
 
     try {
       const response = await fetch(
         `https://scrumserver.tenobe.org/scrum/api/bericht/read.php?profielId=${id}`
       );
-      const conversations = await response.json();
 
-      //TODO: split this up in functions
-      let userIds = conversations.map(([{ partnerId }]) => partnerId);
-      
-      //Do we need to make a new conversation?
-      if (url.searchParams.has("id")) {
-        //check if we aren't already talking to this person
-        const contactId = url.searchParams.get("id");
-        console.log(userIds);
-        if(userIds.includes(contactId)) {
-          selectedUser = userIds.indexOf(contactId);
-          this.changeContact(selectedUser);
-          console.log(selectedUser);
-        } else {
-          //Add him to the end of the list
-          userIds = [userIds, url.searchParams.get("id")];
-        }
-      }
-
-      let contacts = await Promise.all(
-        userIds.map(async id => {
-          const response = await fetch(
-            `https://scrumserver.tenobe.org/scrum/api/profiel/read_one.php?id=${id}`
-          );
-          return response.json();
-        })
-      );
-
-      contacts = contacts.map(({ familienaam, voornaam, foto, id }) => ({
-        familienaam,
-        voornaam,
-        foto,
-        id
-      }));
-
-      this.setState({
-        conversations,
-        conversation: conversations[selectedUser],
-        contacts
-      });
-
-      //TODO:scroll to the bottom of the chat.
-      //element.scrollIntoView(false);
+      return await response.json();
     } catch ({ message }) {
       console.log(message);
     }
   };
 
+  createNewConversation = (initialConversations, initialUserIds) => {
+    const url = new URL(window.location.href);
+    let selectedUser = this.state.selectedUser;
+    let conversations = initialConversations;
+    let userIds = initialUserIds;
+
+    //TODO: Should probable use a statemachine for this one.....
+    // Check if the user was linked to the chat
+    if (url.searchParams.has("id")) {
+      const contactId = url.searchParams.get("id");
+
+      //check if there is already a selected user in session.
+      if (sessionStorage.getItem("selectedUser")) {
+        const sessionSelectedUser = sessionStorage.getItem("selectedUser");
+        // Get the id of the user
+        const sessionUserId = userIds[sessionSelectedUser];
+        // Compare ids
+        if (sessionUserId == contactId) {
+          selectedUser = sessionSelectedUser;
+        }
+      } else {
+        if (userIds.includes(contactId)) {
+          selectedUser = userIds.indexOf(contactId);
+        } else {
+          //Add him to the end of the user list
+          userIds = [...userIds, url.searchParams.get("id")];
+
+          userIds.sort((a, b) => a - b);
+
+          //Selecte the new user
+          selectedUser = userIds.indexOf(contactId);
+
+          //Add empty conversation to list.
+          conversations = [
+            ...conversations.slice(0, selectedUser),
+            [],
+            ...conversations.slice(selectedUser)
+          ];
+        }
+      }
+    }
+
+    return [conversations, selectedUser, userIds];
+  };
+
+  getContacts = async userIds => {
+    //TODO: Probably can clean this up a bit.
+    const contacts = await Promise.all(
+      userIds.map(async id => {
+        const response = await fetch(
+          `https://scrumserver.tenobe.org/scrum/api/profiel/read_one.php?id=${id}`
+        );
+        return response.json();
+      })
+    );
+
+    return contacts.map(({ familienaam, voornaam, foto, id, email }) => ({
+      familienaam,
+      voornaam,
+      foto,
+      id,
+      email
+    }));
+  };
+
   changeContact = selectedUser => {
     sessionStorage.setItem("selectedUser", selectedUser);
 
-    this.setState(({ conversations }) => ({
-      selectedUser,
-      conversation: conversations[parseInt(selectedUser)]
-    }));
+    this.setState({ selectedUser });
   };
 
   onTextChange = e => {
@@ -191,7 +258,6 @@ class App extends Component {
   };
 
   onTextSend = async () => {
-    //send text.
     const { text, contacts, selectedUser } = this.state;
     const url = "https://scrumserver.tenobe.org/scrum/api/bericht/post.php";
 
@@ -214,21 +280,16 @@ class App extends Component {
     });
 
     try {
-      const response = await fetch(request);
+      await fetch(request);
+      const conversations = await this.getConversations();
 
-      //Clear text
-      this.setState({ text: "" });
-      this.getConversations();
-      //console.log(`message: ${message} | id=${id}`);
+      this.setState({ text: "", conversations });
     } catch ({ message }) {
       alert(message);
     }
   };
 
-  render(
-    {},
-    { contacts = [], conversations = [], conversation = [], selectedUser }
-  ) {
+  render({}, { contacts = [], conversations = [], selectedUser, text }) {
     if (conversations.length === 0) {
       return html`
         <div class="verlegen">
@@ -245,9 +306,10 @@ class App extends Component {
           selectedUser=${selectedUser}
         />
         <${Chat}
-          conversation=${conversation}
+          conversation=${conversations[selectedUser]}
           onTextChange=${this.onTextChange}
           onTextSend=${this.onTextSend}
+          text=${text}
         />
       </div>
     `;
